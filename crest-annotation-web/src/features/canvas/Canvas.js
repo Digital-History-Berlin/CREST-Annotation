dc0a6e367ff9a979da0de6021162cbb040187ecb
Andre <Andre_Himmelspach@gmx.de>
2022-11-22 16:12:18 +0100

    feat: add bounding-boxes as new drawing tool

--- crest-annotation-web/src/features/canvas/Canvas.js
+++ crest-annotation-web/src/features/canvas/Canvas.js
@@ -1,17 +1,28 @@
 import React from "react";
-import { Stage, Layer, Line, Text } from "react-konva";
+import { Stage, Layer, Line, Text, Rect } from "react-konva";
 import { BackgroundImage } from "../backgroundImage/BackgroundImage";
 
 export function Canvas() {
   const [tool, setTool] = React.useState("pen");
   const [lines, setLines] = React.useState([]);
+  const [boundingBoxes, setBoundingBoxes] = React.useState([]);
+  const [newBoundingBox, setNewBoundingBox] = React.useState([]);
 
   const isDrawing = React.useRef(false);
 
   const handleMouseDown = (e) => {
-    isDrawing.current = true;
-    const pos = e.target.getStage().getPointerPosition();
-    setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+    if (tool === "pen" || tool === "eraser") {
+      isDrawing.current = true;
+      const pos = e.target.getStage().getPointerPosition();
+      setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+    } else if (tool === "boundingBox") {
+      // if just started drawing a bounding box
+      if (isDrawing.current === false) {
+        isDrawing.current = true;
+        const { x, y } = e.target.getStage().getPointerPosition();
+        setNewBoundingBox([{ x, y, width: 0, height: 0, key: "-1" }]);
+      }
+    }
   };
 
   const handleMouseMove = (e) => {
@@ -19,21 +30,56 @@ export function Canvas() {
     if (!isDrawing.current) {
       return;
     }
-    const stage = e.target.getStage();
-    const point = stage.getPointerPosition();
-    let lastLine = lines[lines.length - 1];
-    // add point
-    lastLine.points = lastLine.points.concat([point.x, point.y]);
+    if (tool === "pen" || tool === "eraser") {
+      const stage = e.target.getStage();
+      const point = stage.getPointerPosition();
+      let lastLine = lines[lines.length - 1];
+      // add point
+      lastLine.points = lastLine.points.concat([point.x, point.y]);
 
-    // replace last
-    lines.splice(lines.length - 1, 1, lastLine);
-    setLines(lines.concat());
+      // replace last
+      lines.splice(lines.length - 1, 1, lastLine);
+      setLines(lines.concat());
+    } else if (tool === "boundingBox") {
+      if (isDrawing.current === true) {
+        // update so user can see the bounding box live
+        const sx = newBoundingBox[0].x;
+        const sy = newBoundingBox[0].y;
+        const { x, y } = e.target.getStage().getPointerPosition();
+        setNewBoundingBox([
+          {
+            x: sx,
+            y: sy,
+            width: x - sx,
+            height: y - sy,
+            key: "-1",
+          },
+        ]);
+      }
+    }
   };
 
-  const handleMouseUp = () => {
+  const handleMouseUp = (e) => {
     isDrawing.current = false;
+    if (tool === "boundingBox") {
+      const sx = newBoundingBox[0].x;
+      const sy = newBoundingBox[0].y;
+      const { x, y } = e.target.getStage().getPointerPosition();
+      const boundingBoxToAdd = {
+        x: sx,
+        y: sy,
+        width: x - sx,
+        height: y - sy,
+        // give it a unique key when finished drawing
+        key: boundingBoxes.length + 1,
+      };
+      boundingBoxes.push(boundingBoxToAdd);
+      setBoundingBoxes(boundingBoxes);
+      setNewBoundingBox([]);
+    }
   };
 
+  const boundingBoxesToDraw = boundingBoxes.concat(newBoundingBox);
   return (
     <div>
       <Stage
@@ -46,6 +92,16 @@ export function Canvas() {
         <Layer>
           <BackgroundImage />
           <Text text="Just start drawing" x={5} y={30} />
+          {boundingBoxesToDraw.map((value) => (
+            <Rect
+              x={value.x}
+              y={value.y}
+              width={value.width}
+              height={value.height}
+              fill={"transparent"}
+              stroke={"red"}
+            />
+          ))}
           {lines.map((line, i) => (
             <Line
               key={i}
@@ -70,6 +126,7 @@ export function Canvas() {
       >
         <option value="pen">Pen</option>
         <option value="eraser">Eraser</option>
+        <option value="boundingBox">Bounding-Box</option>
       </select>
     </div>
   );
