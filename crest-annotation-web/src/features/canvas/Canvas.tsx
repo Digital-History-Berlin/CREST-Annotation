8ac48bae58701337fb62985a02787e056163a017
Andre <Andre_Himmelspach@gmx.de>
2022-12-01 16:52:11 +0100

    fix: use events correctly and fix bug that did not show boundingboxes while drawing them

--- crest-annotation-web/src/features/canvas/Canvas.tsx
+++ crest-annotation-web/src/features/canvas/Canvas.tsx
@@ -5,24 +5,24 @@ import Konva from "konva";
 import KonvaEventObject = Konva.KonvaEventObject;
 
 export function Canvas() {
-  type BoundingBox = {
+  interface BoundingBox {
     x: number;
     y: number;
     width: number;
     height: number;
     key: number;
-  };
-  type BoundingCircle = {
+  }
+  interface BoundingCircle {
     x: number;
     y: number;
     radius: number;
     key: number;
-  };
-  type BoundingLine = {
+  }
+  interface BoundingLine {
     tool: string;
     points: number[];
     key: number;
-  };
+  }
 
   const [tool, setTool] = React.useState<string>("pen");
   const [lines, setLines] = React.useState<BoundingLine[]>([]);
@@ -36,55 +36,75 @@ export function Canvas() {
   >([]);
   const isDrawing = React.useRef(false);
 
-  const handleMouseDown = (event: KonvaEventObject<MouseEvent>) => {
-    const x = event.evt.x;
-    const y = event.evt.y;
+  const handleMouseDown = (
+    event: KonvaEventObject<MouseEvent | TouchEvent>
+  ) => {
+    const pos = event.target.getStage()?.getPointerPosition();
+    if (pos === undefined || pos === null) return;
 
     if (tool === "pen" || tool === "eraser") {
       isDrawing.current = true;
-      const newLine: BoundingLine = { tool, points: [x, y], key: -1 };
+      const newLine: BoundingLine = { tool, points: [pos.x, pos.y], key: -1 };
       setLines([...lines, newLine]);
     } else if (tool === "boundingBox") {
       // if just started drawing a bounding box
       if (!isDrawing.current) {
         isDrawing.current = true;
-        const newBoundingBox: BoundingBox = {x, y, width: 0, height: 0, key: -1};
+        const newBoundingBox: BoundingBox = {
+          x: pos.x,
+          y: pos.y,
+          width: 0,
+          height: 0,
+          key: -1,
+        };
         setNewBoundingBox([newBoundingBox]);
       }
     } else if (tool === "boundingCircle") {
       // if just started drawing a bounding circle
       if (!isDrawing.current) {
         isDrawing.current = true;
-        const newBoundingCircle: BoundingCircle = {x, y, radius: 0, key: -1};
+        const newBoundingCircle: BoundingCircle = {
+          x: pos.x,
+          y: pos.y,
+          radius: 0,
+          key: -1,
+        };
         setNewBoundingCircle([newBoundingCircle]);
       }
     }
   };
 
-  const handleMouseMove = (event: KonvaEventObject<MouseEvent>) => {
-    // console.log("mouse move");
+  const handleMouseMove = (
+    event: KonvaEventObject<MouseEvent | TouchEvent>
+  ) => {
     // no drawing - skipping
     if (!isDrawing.current) {
       return;
     }
 
-    const x = event.evt.x;
-    const y = event.evt.y;
+    const pos = event.target.getStage()?.getPointerPosition();
+    if (pos === undefined || pos === null) return;
 
     if (tool === "pen" || tool === "eraser") {
       let lastLine = lines[lines.length - 1];
       // add point
-      lastLine.points = lastLine.points.concat([x, y]);
+      lastLine.points = lastLine.points.concat([pos.x, pos.y]);
 
       // replace last
       lines.splice(lines.length - 1, 1, lastLine);
       setLines(lines.concat());
     } else if (tool === "boundingBox") {
-      if (!isDrawing.current) {
+      if (isDrawing.current) {
         // update so user can see the bounding box live
         const old_x = newBoundingBox[0].x;
         const old_y = newBoundingBox[0].y;
-        const updatedBoundingBox: BoundingBox = {x: old_x, y: old_y, width: x - old_x, height: y - old_y, key: -1,};
+        const updatedBoundingBox: BoundingBox = {
+          x: old_x,
+          y: old_y,
+          width: pos.x - old_x,
+          height: pos.y - old_y,
+          key: -1,
+        };
         setNewBoundingBox([updatedBoundingBox]);
       }
     } else if (tool === "boundingCircle") {
@@ -92,51 +112,52 @@ export function Canvas() {
         // update so user can see the bounding circle live
         const old_x = newBoundingCircle[0].x;
         const old_y = newBoundingCircle[0].y;
-        const updatedBoundingCircle: BoundingCircle ={
+        const updatedBoundingCircle: BoundingCircle = {
           x: old_x,
           y: old_y,
-          radius: Math.sqrt(Math.pow(x - old_x, 2) + Math.pow(y - old_y, 2)),
+          radius: Math.sqrt(
+            Math.pow(pos.x - old_x, 2) + Math.pow(pos.y - old_y, 2)
+          ),
           key: -1,
-        }
+        };
         setNewBoundingCircle([updatedBoundingCircle]);
       }
     }
   };
 
-  const handleMouseUp = (event: KonvaEventObject<MouseEvent>) => {
+  const handleMouseUp = (event: KonvaEventObject<MouseEvent | TouchEvent>) => {
     isDrawing.current = false;
-    const x = event.evt.x;
-    const y = event.evt.y;
+    const pos = event.target.getStage()?.getPointerPosition();
+    if (pos === undefined || pos === null) return;
 
-    if(tool === "pen" || tool === "eraser") {
-      console.log("here");
+    if (tool === "pen" || tool === "eraser") {
       let lastLine = lines[lines.length - 1];
-      lastLine.key = lines.length -1;
+      lastLine.key = lines.length - 1;
       // replace last
       lines.splice(lines.length - 1, 1, lastLine);
       setLines(lines.concat());
-
     } else if (tool === "boundingBox") {
       const old_x = newBoundingBox[0].x;
       const old_y = newBoundingBox[0].y;
       const boundingBoxToAdd: BoundingBox = {
         x: old_x,
         y: old_y,
-        width: x - old_x,
-        height: y - old_y,
+        width: pos.x - old_x,
+        height: pos.y - old_y,
         // give it a unique key when finished drawing
         key: boundingBoxes.length,
       };
       setBoundingBoxes([...boundingBoxes, boundingBoxToAdd]);
       setNewBoundingBox([]);
-
     } else if (tool === "boundingCircle") {
       const old_x = newBoundingCircle[0].x;
       const old_y = newBoundingCircle[0].y;
-      const boundingCircleToAdd : BoundingCircle = {
+      const boundingCircleToAdd: BoundingCircle = {
         x: old_x,
         y: old_y,
-        radius: Math.sqrt(Math.pow(x - old_x, 2) + Math.pow(y - old_y, 2)),
+        radius: Math.sqrt(
+          Math.pow(pos.x - old_x, 2) + Math.pow(pos.y - old_y, 2)
+        ),
         // give it a unique key when finished drawing
         key: boundingCircles.length,
       };
@@ -155,9 +176,9 @@ export function Canvas() {
         onMouseDown={handleMouseDown}
         onMousemove={handleMouseMove}
         onMouseup={handleMouseUp}
-        //onTouchStart={handleMouseDown}
-        //onTouchMove={handleMouseMove}
-        //onTouchEnd={handleMouseUp}
+        onTouchStart={handleMouseDown}
+        onTouchMove={handleMouseMove}
+        onTouchEnd={handleMouseUp}
       >
         <Layer>
           <BackgroundImage />
