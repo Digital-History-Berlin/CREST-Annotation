7dd5ac324c6657df86b331505ba9492083b8f856
Andre <Andre_Himmelspach@gmx.de>
2023-03-12 14:34:06 +0100

    feat: add editing of rectangle width/height

--- crest-annotation-web/src/features/annotate/components/tools/Rectangle.tsx
+++ crest-annotation-web/src/features/annotate/components/tools/Rectangle.tsx
@@ -16,19 +16,109 @@ const Rectangle = ({
 }: ShapeProps) => {
   const rectangle = annotation.shape as RectangleShape;
 
-  const onDragCenter = (e: Konva.KonvaEventObject<DragEvent>) => {
+  const editingPoints = [
+    {
+      x: rectangle.x + rectangle.width / 2,
+      y: rectangle.y + rectangle.height / 2,
+    },
+    { x: rectangle.x, y: rectangle.y },
+    { x: rectangle.x + rectangle.width / 2, y: rectangle.y },
+    { x: rectangle.x + rectangle.width, y: rectangle.y },
+    { x: rectangle.x + rectangle.width, y: rectangle.y + rectangle.height / 2 },
+    { x: rectangle.x + rectangle.width, y: rectangle.y + rectangle.height },
+    { x: rectangle.x + rectangle.width / 2, y: rectangle.y + rectangle.height },
+    { x: rectangle.x, y: rectangle.y + rectangle.height },
+    { x: rectangle.x, y: rectangle.y + rectangle.height / 2 },
+  ];
+
+  const onDragEditPoint = (
+    e: Konva.KonvaEventObject<DragEvent>,
+    index: number
+  ) => {
     const shape = annotation.shape;
     if (shape === undefined) return;
 
     const pos = getTransformedPointerPosition(e);
     if (pos === undefined) return;
 
+    let changes = {};
+
+    switch (index) {
+      // center
+      case 0:
+        changes = {
+          x: pos.x - rectangle.width / 2,
+          y: pos.y - rectangle.height / 2,
+        };
+        break;
+      // top left
+      case 1:
+        changes = {
+          x: pos.x,
+          y: pos.y,
+          width: rectangle.x + rectangle.width - pos.x,
+          height: rectangle.y + rectangle.height - pos.y,
+        };
+        break;
+      // top middle
+      case 2:
+        // lock edit point in x-axis, only move in y-axis
+        e.target.x(rectangle.x + rectangle.width / 2);
+        changes = {
+          y: pos.y,
+          height: rectangle.y + rectangle.height - pos.y,
+        };
+        break;
+      // top right
+      case 3:
+        changes = {
+          y: pos.y,
+          height: rectangle.y + rectangle.height - pos.y,
+          width: pos.x - rectangle.x,
+        };
+        break;
+      // right middle
+      case 4:
+        // lock edit point in y-axis, only move in x-axis
+        e.target.y(rectangle.y + rectangle.height / 2);
+        changes = { width: pos.x - rectangle.x };
+        break;
+      // bottom right
+      case 5:
+        changes = {
+          width: pos.x - rectangle.x,
+          height: pos.y - rectangle.y,
+        };
+        break;
+      // bottom middle
+      case 6:
+        // lock edit point in x-axis, only move in y-axis
+        e.target.x(rectangle.x + rectangle.width / 2);
+        changes = { height: pos.y - rectangle.y };
+        break;
+      // bottom left
+      case 7:
+        changes = {
+          x: pos.x,
+          width: rectangle.x + rectangle.width - pos.x,
+          height: pos.y - rectangle.y,
+        };
+        break;
+      // left middle
+      case 8:
+        // lock edit point in y-axis, only move in x-axis
+        e.target.y(rectangle.y + rectangle.height / 2);
+        changes = {
+          x: pos.x,
+          width: rectangle.x + rectangle.width - pos.x,
+        };
+    }
+
     onUpdate?.({
       ...annotation,
       shape: {
         ...shape,
-        x: pos.x - rectangle.width / 2,
-        y: pos.y - rectangle.height / 2,
+        ...changes,
       },
     });
   };
@@ -43,18 +133,19 @@ const Rectangle = ({
         width={rectangle.width}
         height={rectangle.height}
       />
-      {editing && (
-        <Circle
-          x={rectangle.x + rectangle.width / 2}
-          y={rectangle.y + rectangle.height / 2}
-          radius={5}
-          fill={alpha(color, 0.8)}
-          draggable
-          onDragMove={(e) => {
-            onDragCenter(e);
-          }}
-        />
-      )}
+      {editing &&
+        editingPoints.map((editPoint, index) => (
+          <Circle
+            x={editPoint.x}
+            y={editPoint.y}
+            radius={5}
+            fill={alpha(color, 0.8)}
+            draggable
+            onDragMove={(e) => {
+              onDragEditPoint(e, index);
+            }}
+          />
+        ))}
     </Group>
   );
 };
